7ab36aea02ac9101ebfd8a43c734ccb9d1854bb7
Leofred Selga Nuñez <143275696+devilyor25@users.noreply.github.com>
2023-09-22 23:48:21 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#element', {
+    strings: ['<i>Computer</i> Technian.', 'Computer Science 3rd Year Student'],
+    typeSpeed: 50,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+  });
